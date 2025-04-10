c274153b1b54618a38240600ca1a43df7f866f67
smac330 <shakaylahmacklin330@gmail.com>
2025-04-10 18:12:51 -0400

    feat: added a function to insert into the list

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,18 @@ impl<T: PartialEq + Clone> DynamicLinkedList<T> {
     pub fn new() -> Self {
         Self { head: None }
     }
+
+    pub fn insert(&mut self, data: T) {
+        let mut new_node = Box::new(Node { data, next: None });
+        let mut curr = &mut self.head;
+
+        while let Some(node) = curr {
+            curr = &mut node.next;
+        }
+
+        *curr = Some(new_node);
+    }
+
 }
 
 mod test; 
